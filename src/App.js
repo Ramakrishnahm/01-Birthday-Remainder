97cca9ab694377fccaed3bd2e30d6a6ce0dbb9d1
ramakrishnahm <ramakrishna729m@gmail.com>
2023-06-25 13:19:40 +0530

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
+import data from './Data';
+import List from './List'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function App(){
+  const[people,setPeople] = useState(data)
+  return(
+    <main>
+      <section className = 'container'>
+        <h3>{people.length}birthdays today</h3>
+        <List people = {people}/>
+
+        <button 
+        type = 'buttton'
+        className = 'btn btn-block'
+        onClick = {() =>setPeople([])}>
+          clear All
+        </button>
+
+      </section>
+    </main>
+  )
 }
 
 export default App;
