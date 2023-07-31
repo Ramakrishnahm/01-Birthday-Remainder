0d4e81961f736c2b4d9ae9874464c6d01daad215
Ramakrishnahm <97603079+Ramakrishnahm@users.noreply.github.com>
2023-07-31 15:52:11 +0530

    Body
    i  have add 4 files

---
+++ Body.js
@@ -0,0 +1,71 @@
+import { restaurantList } from "../contants";
+import RestaurantCard from "./RestaurantCard";
+import { useState,useEffect } from "react";
+
+// What is state
+// what is React Hooks? - functions,
+// What is useState
+
+function filterData(searchText, restaurants) {
+  const filterData = restaurants.filter((restaurant) =>
+    restaurant.data.name.includes(searchText)
+  );
+
+  return filterData;
+}
+
+const Body = () => {
+  const [restaurants, setRestaurants] = useState(restaurantList);
+  const [searchText, setSearchText] = useState("");
+
+ 
+
+  useEffect(() => {
+    // API call
+    getRestaurants();
+  }, []);
+
+  async function getRestaurants() {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    console.log(json);
+
+  return (
+    <>
+      <div className="search-container">
+        <input
+          type="text"
+          className="search-input"
+          placeholder="Search"
+          value={searchText}
+          onChange={(e) => {
+            setSearchText(e.target.value);
+          }}
+        />
+        <button
+          className="search-btn"
+          onClick={() => {
+            //need to filter the data
+            const data = filterData(searchText, restaurants);
+            // update the state - restaurants
+            setRestaurants(data);
+          }}
+        >
+          Search
+        </button>
+      </div>
+      <div className="restaurant-list">
+        {restaurants.map((restaurant) => {
+          return (
+            <RestaurantCard {...restaurant.data} key={restaurant.data.id} />
+          );
+        })}
+      </div>
+    </>
+  );
+}
+};
+
+export default Body
